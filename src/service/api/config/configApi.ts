effc8e3ad1ecb534fe3aeb3a9edb91a5126edf0a
guilherme <guivillarinho15@gmail.com>
2025-11-01 16:13:34 -0300

    alterando baseURL no config axios

--- src/service/api/config/configApi.ts
+++ src/service/api/config/configApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://apihd.cinbal.com.br',
+  'baseURL':'http://localhost:3545',
   // baseURL: 'http://localhost:8181',
 })
